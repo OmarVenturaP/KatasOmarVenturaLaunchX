ade806b70d830d4920593cabf87c7f9590fa47b1
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-02-14 17:25:09 -0600

    add: files from cloud gui

---
+++ open.py
@@ -0,0 +1,73 @@
+def main():
+    open("/path/to/mars.jpg")
+
+if __name__ == '__main__':
+    main()
+
+try:
+    open('config.txt')
+except FileNotFoundError:
+    print("Couldn't find the config.txt file!")
+
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")
+
+if __name__ == '__main__':
+    main()
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+try:
+    open("mars.jpg")
+except FileNotFoundError as err:
+    print("got a problem trying to read the file:", err)
+
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+try:
+    water_left(5, 100, 2)
+except RuntimeError as err:
+
+def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            argument / 10
+        except TypeError:
+            raise TypeError(f"All arguments must be of type int, but received: '{argument}'")
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
